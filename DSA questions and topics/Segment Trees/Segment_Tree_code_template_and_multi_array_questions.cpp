be05926e3fc499fb3030ffe3d8d5afbaf59d5156
Abhishek <abhishekyadav4538@gmail.com>
2023-07-14 18:38:52 +0530

    14th July 2023

--- DSA questions and topics/Segment Trees/Segment_Tree_code_template_and_multi_array_questions.cpp
+++ DSA questions and topics/Segment Trees/Segment_Tree_code_template_and_multi_array_questions.cpp
@@ -4,7 +4,14 @@
 using namespace std;
 
 /* 
-    Below is the template for 
+    Below is the template for Range Query Data Structure Segment Tree which finds minimum in a range. 
+    This is the optimum choice for problems involving multiple arrays, hence need multiple segment trees
+    corresponding to those arrays. 
+    Here queries can be of the following type (Example of two arrays):
+        Type 1 : We give L1, R1, L2, R2 and we say find the minimum in the range L1 and R1 from array 1
+        and also the minimum in the Range L2 and R2 in array 2. Then finally return the minimum of that.
+        Type 2 : We give three values i.e. arrayNum, ind and the val. Now, we have to update the value at 
+        index ind of the array arrayNum with the value val. 
 */
 
 class SegmentMin{
@@ -18,7 +25,7 @@ class SegmentMin{
 
         void build(int ind, int low, int high, int arr[]){
             if(low == high){
-                seg[ind] == arr[low];
+                seg[ind] = arr[low];
                 return;
             }
 
@@ -30,8 +37,8 @@ class SegmentMin{
 
         int query(int ind, int low, int high, int l, int r){
             if(l > high || r < low) return INT_MAX; // no overlap
-            else if(low <= l && r <= high) return seg[ind]; // complete overlap
-            else{
+            else if(low >= l && r >= high) return seg[ind]; // complete overlap
+            else{                                           // partial overlap
                 int mid = low + (high-low)/2;
                 int left = query(2*ind+1, low, mid, l, r);
                 int right = query(2*ind+2, mid+1, high, l, r);
@@ -40,7 +47,7 @@ class SegmentMin{
         }
 
         void update(int ind, int low, int high, int i, int value){    // point update
-            if(low == high == i){
+            if(low == high){
                 seg[ind] = value;
                 return;
             }
@@ -50,24 +57,68 @@ class SegmentMin{
 
             seg[ind] = min(seg[2*ind+1], seg[2*ind+2]);
         }  
+
+        int arrayMin(){
+            return seg[0];
+        }
 };
 
 int main(){
-    int n;
-    cout<<"Enter the number of elements of the array: "<<endl;
-    cin>>n;
+    int n1, n2;
+    cout<<"Enter the number of elements of the first and second array: "<<endl;
+    cin>>n1>>n2;
 
-    int arr[n];
-    cout<<"Enter the array elements: "<<endl;
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    int arr1[n1], arr2[n2];
+    cout<<"Enter the first array elements: "<<endl;
+    for(int i=0; i<n1; i++){
+        cin>>arr1[i];
     }
 
-    SegmentMin s1(n);
-    s1.build(0, 0, n-1, arr);
-    cout<<"Segment Tree Build complete!"<<endl;
+    cout<<"Enter the second array elements: "<<endl;
+    for(int i=0; i<n2; i++){
+        cin>>arr2[i];
+    }
 
-    
+    SegmentMin s1(n1), s2(n2);
+    s1.build(0, 0, n1-1, arr1);
+    cout<<"First Segment Tree Build complete! "<<s1.arrayMin()<<endl;
+    s2.build(0, 0, n2-1, arr2);    
+    cout<<"Second Segment Tree Build complete! "<<s2.arrayMin()<<endl;
+
+    int t; 
+    cout<<"Enter the number of testcases: "<<endl;
+    cin>>t;
+
+    while(t--){
+        int typeofQuery;
+        cout<<"\nEnter the Query type: "<<endl;
+        cin>>typeofQuery;
+        if(typeofQuery == 1){
+            int l1, r1, l2, r2;
+            cout<<"Enter the first and second index of the first array: "<<endl;
+            cin>>l1>>r1;
+            cout<<"Enter the first and second index of the second array: "<<endl; 
+            cin>>l2>>r2;
+            int min1 = s1.query(0, 0, n1-1, l1, r1);
+            int min2 = s2.query(0, 0, n2-1, l2, r2);
+            cout<<"Minimum in the range "<<l1<<" to "<<r1<<" of the first array is: "<<min1<<" and in " 
+            "the range "<<l2<<" to "<<r2<<" of the second array is: "<<min2<<"\nHence, the overall"
+            " minimum of the two is: "<<min(min1, min2);
+        }
+        else if(typeofQuery == 2){
+            int arrayNum, ind, val;
+            cout<<"Enter the array, index and the value which you want to update: "<<endl;
+            cin>>arrayNum>>ind>>val;
+            if(arrayNum == 1){
+                s1.update(0, 0, n1-1, ind, val);
+                arr1[ind] = val;
+            }
+            else if(arrayNum == 2){
+                s2.update(0, 0, n2-1, ind, val);
+                arr2[ind] = val;
+            }
+        }
+    }
 
     return 0;
 }
